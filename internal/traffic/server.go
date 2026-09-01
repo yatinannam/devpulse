31e001cfe824c1bdf98bc270e942282627847b1d
Yatin Annam <ninjayatin@gmail.com>
2026-09-01 11:30:05 +0530

    fix: report proxy bind errors synchronously

--- internal/traffic/server.go
+++ internal/traffic/server.go
@@ -2,15 +2,28 @@ package traffic
 
 import (
 	"context"
+	"fmt"
+	"net"
 	"net/http"
 )
 
-func Start(proxy http.Handler, addr string) *http.Server {
+func Start(proxy http.Handler, addr string) (*http.Server, error) {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return nil, fmt.Errorf("listen on %s: %w", addr, err)
+	}
 	server := &http.Server{Addr: addr, Handler: proxy}
-	go func() { _ = server.ListenAndServe() }()
-	return server
+	go func() { _ = server.Serve(listener) }()
+	return server, nil
 }
 
 func Shutdown(server *http.Server) error {
+	if server == nil {
+		return nil
+	}
 	return server.Shutdown(context.Background())
+}
+
+func Serve(proxy http.Handler, addr string) error {
+	return http.ListenAndServe(addr, proxy)
 }
